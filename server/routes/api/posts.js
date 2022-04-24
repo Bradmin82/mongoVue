f2ac5384ed482b73a40e00f2a4a47dff33f34985
Bradmin82 <bweldy82@gmail.com>
2022-04-24 13:34:32 -0700

    before creating dev branch

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -20,13 +20,14 @@ router.post('/', async (req, res) => {
 });
 
 // Update Post
-router.put('/:id', async (req, res) => {
+// router.patch('/:id', getPost, async (req, res) => {
+router.patch('/:id', async (req, res) => {
   const posts = await loadPostsCollection();
   console.log('posts, res.status(200)', res.status);
   await posts.updateOne({
     _id: new mongodb.ObjectId(req.params.id),
-    text: req.body.newText,
-    updatedAt: new Date()
+    $set: {text: req.body.newText},
+    // updatedAt: new Date()
   });
   res.status(201 || 200).send();
 });
@@ -37,6 +38,20 @@ router.delete('/:id', async (req, res) => {
   await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)})
   res.status(200).send();
 });
+
+async function getPost(req, res, next) {
+  try {
+    posts = await Post.findById(req.params.id)
+    if (post == null ) {
+      return res.status(404).json({ message: 'Cannot find post' })
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+
+  res.post = post
+  next()
+}
 
 async function loadPostsCollection() {
   const client = await mongodb.MongoClient.connect('mongodb+srv://root:toor@vuemongo.guvtq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
